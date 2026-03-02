3e03d93c5f1785ef2fe3032a99665849bb844398
Haysansan <haysan.eang@student.cadt.edu.kh>
2026-03-02 16:21:00 +0700

    2. Update AppSettingsState

--- lib/ui/states/settings_state.dart
+++ lib/ui/states/settings_state.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/widgets.dart';
- 
+
+import '../../data/repositories/settings/app_settings_repository.dart';
 import '../../model/settings/app_settings.dart';
 
 class AppSettingsState extends ChangeNotifier {
- 
+  final AppSettingsRepository _repository;
+
   AppSettings? _appSettings;
- 
-  AppSettingsState();
+
+  AppSettingsState({required AppSettingsRepository repository})
+    : _repository = repository;
 
   Future<void> init() async {
-    // Might be used to load data from repository
+    _appSettings = await _repository.load();
+    notifyListeners();
   }
 
+  // Might be used to load data from repository
   ThemeColor get theme => _appSettings?.themeColor ?? ThemeColor.blue;
 
   Future<void> changeTheme(ThemeColor themeColor) async {
     if (_appSettings == null) return;
     _appSettings = _appSettings!.copyWith(themeColor: themeColor);
-
+    await _repository.save(_appSettings!);
     notifyListeners();
   }
 }
